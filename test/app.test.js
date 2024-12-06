296fce32cbea0f35513ccf3efb0ee6822ec114fd
Alejandro1ro <kelwingerman48@gmail.com>
2024-12-05 22:46:21 -0400

    first

--- test/app.test.js
+++ test/app.test.js
@@ -3,9 +3,9 @@ const app = require("../server.mjs");
 
 describe("GET /", () => {
   it("should respond with the index.html file", async () => {
-    const response = await request(app).get("/"); // Hace la solicitud GET a "/"
-    expect(response.status).toBe(200); // Asegura que la respuesta tenga un estado 200
-    expect(response.headers["content-type"]).toContain("text/html"); // Verifica que el tipo de contenido sea HTML
-    expect(response.text).toContain("<!DOCTYPE html>"); // Opcional: valida que el contenido tenga algo esperado de index.html
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+    expect(response.headers["content-type"]).toContain("text/html");
+    expect(response.text).toContain("<!DOCTYPE html>");
   });
 });
