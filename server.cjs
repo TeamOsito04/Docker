fee05c878f3cb43102f7d735d286dabe041e9d34
Alejandro1ro <kelwingerman48@gmail.com>
2024-12-05 23:52:10 -0400

    first

---
+++ server.cjs
@@ -0,0 +1,26 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+const db = new sqlite3.Database("db.sqlite");
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(50))");
+  db.run("INSERT INTO usuarios (nombre) VALUES ('Juan')");
+});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
